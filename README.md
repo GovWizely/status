84bfd5040ab852dbb56cb2735149c28e095d4158
tmhammer <timh@govwizely.com>
2016-06-06 15:41:18 -0400

    Status page

---
+++ README.md
@@ -0,0 +1,77 @@
+# GovWizely Service Status Page
+
+This project is a single-page Jekyll site to display the status of GovWizely services, including Pingdom uptime checks and Browserstack screenshots.
+
+## Local Installation Requirements
+
+- Linux, Unix, Windows or Mac OS X
+- [Ruby](http://www.ruby-lang.org/en/downloads/)
+- [RubyGems](http://rubygems.org/pages/download)
+- [Jekyll](http://jekyllrb.com)
+
+
+## Ruby
+
+### To install ruby on unix:
+
+`yum install ruby` (or `sudo apt-get install ruby2.2.1`)
+
+
+### To install ruby on Mac OS X:
+
+`curl -L https://get.rvm.io | bash -s stable --ruby`
+
+Visit the following links for more detailed information on how to set up Ruby using a method applicable to your environment:
+
+Three Ways of Installing Ruby (Linux/Unix)
+http://www.ruby-lang.org/en/downloads/
+ 
+RubyInstaller for Windows
+http://rubyinstaller.org/
+
+How to Install Ruby on a Mac
+http://net.tutsplus.com/tutorials/ruby/how-to-install-ruby-on-a-mac/
+
+
+## Install rubygems: 
+
+- `cd ~/`
+- `wget http://production.cf.rubygems.org/rubygems/rubygems-2.4.6.tgz`
+- `tar xzvf rubygems-2.4.6.tgz`
+- `cd rubygems-2.4.6`
+- `ruby setup.rb`
+
+
+## Managing Dependencies Using Bundler
+
+We recommend using Bundler to manage dependencies. Once you have Ruby installed, install Bundler by running the following command: 'gem install bundler'
+
+Once Bundler is installed, you install/update dependencies by simply running 'bundle install' within your project folder.
+
+More information on Bundler may be found here: http://gembundler.com/
+
+
+## Install Jekyll
+
+- `cd WebSite` (or the location of your cloned repository)
+- `bundle install`
+
+For more information and detailed documentation on Jekyll, visit the following sites:
+
+Jekyll Project Home
+http://jekyllrb.com
+
+Jekyll on GitHub
+https://github.com/mojombo/jekyll
+
+
+## Clone the repository
+
+- `cd /var/www/html` (or the location you would like the compiled site to live)
+- `git clone https://github.com/GovWizely/status.git`
+
+
+## Generate the site and serve
+
+- `bundle exec jekyll serve`
+- Browse to [localhost:4000/stats/](http://localhost:4000/status/) to view the site
---
+++ get_screenshots.rb
@@ -0,0 +1,53 @@
+require 'screenshot'
+require 'open-uri'
+require 'yaml'
+
+settings = {:username => ENV["BROWSERSTACK_USERNAME"], :password => ENV["BROWSERSTACK_PASSWORD"]}
+client = Screenshot::Client.new(settings)
+
+# Grab latest list of possible browsers for reference
+File.open("all_possible_browsers.yml", 'w'){|f| f.write(client.get_os_and_browsers.to_yaml)}
+
+# To modify the browsers getting tested, update the browser hash below, along with the 
+#    corresponding HTML in _posts/2016-05-26-update.md
+params = {
+    :url => "https://success.export.gov/search#/search?q=trade&_k=owl0t9",
+    :win_res => "1280x1024",     #Options : "1024x768", "1280x1024"
+    :mac_res => "1920x1080",    #Options : "1024x768", "1280x960", "1280x1024", "1600x1200", "1920x1080"
+    :quality => "compressed",   #Options : "compressed", "original"
+    :wait_time => 5,            #Options: 2, 5, 10, 15, 20, 60
+    :orientation => "portrait", #Options: "portrait", "landscape"
+    :tunnel => false,
+    :browsers => [
+            { os: "Windows", os_version: "10", browser: "ie", browser_version: "11.0" },
+            { os: "Windows", os_version: "10", browser: "firefox", browser_version: "45.0" },
+            { os: "Windows", os_version: "10", browser: "chrome", browser_version: "50.0" },
+            { os: "Windows", os_version: "7", browser: "ie", browser_version: "10.0" },
+            { os: "OS X", os_version: "El Capitan", browser: "safari", browser_version: "9.1" },
+            { os: "ios", os_version: "8.3", device: "iPhone 6"},
+            { os: "ios", os_version: "5.0", device: "iPad 2 (5.0)"},
+            { os: "android", os_version: "4.4", device: "Samsung Galaxy S5"}
+    ]
+}
+
+request_id = client.generate_screenshots(params)
+
+while client.screenshots_done?(request_id) == false do
+  puts "Waiting 10 seconds for job to finish..."
+  sleep 10
+end
+
+puts "Waiting 10 more seconds to ensure job is finished..."
+sleep 10
+
+response = client.screenshots(request_id)
+
+screenshot_entries = response.map do |entry|
+  screenshot_name = entry[:os] + " " + entry[:os_version] + " " +  entry[:browser] + " " +  entry[:browser_version]
+  { 'name' => screenshot_name, 'url' => entry[:image_url] }
+end
+
+screenshot_data = { 'screenshot_entries' => screenshot_entries, 'timestamp' => Time.now.strftime("%m/%d/%Y at %l:%M %p")}
+
+File.open("_data/screenshots.yml", 'w'){|f| f.write(screenshot_data.to_yaml)}
+
